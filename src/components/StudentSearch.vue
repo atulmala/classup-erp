d7d322a1feb31485374bf2e7633dc79b05db8d50
Atul Gupta <atul.gupta@clasup.in>
2019-03-03 09:39:06 +0530

    student search and show them in a table (not properly formatted)

--- src/components/StudentSearch.vue
+++ src/components/StudentSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-content>
-      <v-form>
+      <v-form v-if="show_search_criteria">
         <v-container fluid>
           <v-layout row wrap>
             <v-flex xs12 sm6 md3>
@@ -17,7 +17,12 @@
             </v-flex>
 
             <v-flex xs12 sm6 md3>
-              <v-select :items="class_list" label="Class/Standard" v-model="the_class" v-on:focus="dismiss()"></v-select>
+              <v-select
+                :items="class_list"
+                label="Class/Standard"
+                v-model="the_class"
+                v-on:focus="dismiss()"
+              ></v-select>
             </v-flex>
             <div class="text-xs-center">
               <v-btn
@@ -37,6 +42,20 @@
           </v-layout>
         </v-container>
       </v-form>
+      <v-data-table
+        v-if="show_student_list"
+        :headers="headers"
+        :items="students"
+        class="elevation-1"
+      >
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.reg_no }}</td>
+          <td class="text-xs-right">{{ props.item.the_clss }}</td>
+          <td class="text-xs-right">{{ props.item.parent }}</td>
+        </template>
+      </v-data-table>
     </v-content>
   </v-app>
 </template>
@@ -47,71 +66,135 @@ export default {
   name: "StudentSearch",
   data() {
     return {
+      show_search_criteria: true,
+      show_student_list: false,
       loader: null,
       loading: false,
       reg_no: "",
       first_name: "",
       last_name: "",
       the_class: "",
-      class_list: [],
+      class_list: [
+        {
+          'name': 'the_chako',
+          'reg_no': '420',
+          'parent': 'super chako',
+          'the_class': 'IV-C'
+        }
+      ],
+      students: [],
+      alert_type: "",
       alert_message: "",
-      showDismissibleAlert: false
+      showDismissibleAlert: false,
+      headers: [
+        {
+          text: "Student Name",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Reg/Adm/Sch Number", value: "reg_no" },
+        { text: "Class", value: "the_class" },
+        { text: "Parent", value: "parent" }
+      ]
     };
   },
-  mounted: function () {
-    let self = this
-    let school_id = this.$store.getters.get_school_id
-    let ip = this.$store.getters.get_server_ip
-    let url = ip.concat("/academics/class_list/", school_id, "/")
+  mounted: function() {
+    let self = this;
+    let school_id = this.$store.getters.get_school_id;
+    let ip = this.$store.getters.get_server_ip;
+    let url = ip.concat("/academics/class_list/", school_id, "/");
     axios
-          .get(url)
-          .then(function(response) {
-            // handle success
-            self.showDismissibleAlert = true
-            self.alert_message = "class list retrievd"
-            self.class_list = response.data
-            for(var i = 0; i < response.data.length; i++)  {
-              self.class_list[i] = response.data[i]["standard"]
-            }
-            console.log(self.class_list);
-          })
-          .catch(function(error) {
-            // handle error
-            console.log(error);
-          });
+      .get(url)
+      .then(function(response) {
+        // handle success
+        self.class_list = response.data;
+        for (var i = 0; i < response.data.length; i++) {
+          self.class_list[i] = response.data[i]["standard"];
+        }
+        console.log(self.class_list);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
   },
-  watch: {
-    loader() {
-      const l = this.loader;
-      this[l] = !this[l];
 
-      setTimeout(() => (this[l] = false), 3000);
-
-      this.loader = null;
-    }
-  },
   methods: {
     search() {
-      var can_search = true
-      if(this.reg_no == "" && this.first_name == "" ){
-        this.alert_message = "Search criteria provided is NOT Enough."
-        this.showDismissibleAlert = true
-        can_search = false
+      let self = this;
+      var can_search = true;
+      if (this.reg_no == "" && this.first_name == "") {
+        this.alert_message = "Search criteria provided is NOT Enough.";
+        this.showDismissibleAlert = true;
+        can_search = false;
       }
-      if(this.first_name != "" && this.reg_no == "" && this.the_class == "")  {
-        this.alert_message = "Please specify Class"
-        this.showDismissibleAlert = true
-        can_search = false
+      if (this.first_name != "" && this.reg_no == "" && this.the_class == "") {
+        this.alert_message = "Please specify Class";
+        this.showDismissibleAlert = true;
+        can_search = false;
       }
       if (can_search) {
-        
+        let ip = this.$store.getters.get_server_ip;
+        let school_id = this.$store.getters.get_school_id;
+        let url = ip.concat(
+          "/student/list/",
+          school_id,
+          "/",
+          "in_params",
+          "/",
+          "in_params"
+        );
+
+        axios
+          .get(url, {
+            params: {
+              reg_no: this.reg_no,
+              first_name: this.first_name,
+              last_name: this.last_name,
+              the_class: this.the_class
+            }
+          })
+          .then(function(response) {
+            //console.log(response);
+            if (response.data.length == 0) {
+              self.alert_message =
+                "Student not found. Please change the search criteria and try again";
+              self.showDismissibleAlert = true;
+              self.alert_type = "error";
+            } else {
+              self.show_search_criteria = false;
+              for (var i = 0; i < response.data.length; i++) {
+                var student = {};
+                student["reg_no"] = response.data[i]["student_erp_id"];
+                student["name"] =
+                  response.data[i]["fist_name"] +
+                  " " +
+                  response.data[i]["last_name"];
+                student["the_class"] =
+                  response.data[i]["current_class"] +
+                  "-" +
+                  response.data[i]["current_section"];
+                student["parent"] = response.data[i]["parent"];
+                console.log(student)
+                self.students.push(student);
+              }
+              console.log(self.students)
+              self.show_student_list = true;
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+          })
+          .then(function() {
+            // always executed
+          });
       }
-      
-      console.log(this.reg_no)
+      console.log(this.reg_no);
     },
     dismiss() {
       this.showDismissibleAlert = false;
-    },
+    }
   }
 };
 </script>
